346d915d3da5f05e0ba500713151902a19e88b13
Vaidupatel <100674522+Vaidupatel@users.noreply.github.com>
2022-10-02 22:00:37 +0530

    Create operator.c

---
+++ operator.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+int main()
+{
+    // Arithmatic operator
+    
+    // int a;
+    // float b;
+    // a=34;
+    // b=6.34;
+    // printf("a+b= %f\n",a+b);
+    // printf("a*b= %f\n",a*b);
+    // printf("a/b= %f\n",a/b);
+    // printf("a-b= %f\n",a-b);
+    
+    // Relational operator
+
+    // int a,b;
+    // a=34;
+    // b=6;
+    // printf("a==b= %d\n",a==b);
+    // printf("a!=b= %d\n",a!=b);
+    // printf("a>b= %d\n",a>b);
+    // printf("a<b= %d\n",a<b);
+    // printf("a>=b= %d\n",a>=b);
+    // printf("a<=b= %d\n",a<=b);
+    
+
+    // Logical operator
+    // int a,b;
+    // a=34;
+    // b=0;
+    // printf("a&&b= %d\n",a&&b);  /* hearboth are int value than  true */
+    // printf("a||b= %d\n",a||b);  /* hear any one value is int than true */
+    // printf("!b= %d\n",!b);      /* hear if true than reverse and finaly false */
+    
+    // Bitwise operator
+    // (It's operate bit by bit)
+
+    // int a,b;
+    // a=2;
+    // b=3;
+    // printf("a&b= %d\n",a&b);        /*It's chack all bits of a and b and ANDing them and caluculate value */
+    // printf("a|b= %d\n",a|b);        /*It's chack all bits of a and b and ORing them and caluculate value */
+    // printf("a^b= %d\n",a^b);        /*It's chack all bits of a and b and XORing them and caluculate value */
+    //                                 /*(Logic of XOR is --> One is true snd One is false than answer is true) */
+    //  Other bit wise operator is  ( ~ 1's compliment) (>> Shift left) (<< Shift right)
+
+    
+    return 0;
+}
