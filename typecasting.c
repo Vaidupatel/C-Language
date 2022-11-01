011c72170e8b817a53559a6e93f1e0f8116168ce
Vaidupatel <100674522+Vaidupatel@users.noreply.github.com>
2022-11-01 18:34:50 +0530

    Create typecasting.c

---
+++ typecasting.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+     // type casting syntex
+     // (type)value;
+     int a=3;
+     float b=54;
+     printf("the value of the a is %d\n",a);
+     printf("the value of the b is %d\n",(int)b);
+
+     return 0;
+}
