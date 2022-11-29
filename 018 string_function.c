076de4b76f86c687c2d65792bb18ea234235ee0d
Vaidupatel <100674522+Vaidupatel@users.noreply.github.com>
2022-11-29 13:23:40 +0530

    Create 018 string_function.c

---
+++ 018 string_function.c
@@ -0,0 +1,19 @@
+#include<string.h>
+#include<stdio.h>
+int main(){
+
+char s1[]="jaydeep";
+char s2[]="vaidik";
+char s3[54];
+// puts(strcat(s1,s2));
+// printf("The length of s1 is %d\n",strlen(s1)); 
+// printf("The length of s2 is %d\n",strlen(s2)); 
+// printf("The reverse string of s1 is %s\n",strrev(s1)); 
+// printf("The reverse string of s2 is %s\n",strrev(s2)); 
+
+// strcpy(s3,s2);
+// puts(s3);
+printf("The strcp for s1, s2 will return %d\n",strcmp(s1,s2)); 
+
+return 0;
+}
