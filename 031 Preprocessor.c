81fc04334e1565f916b46fbfd4233b6fafd95676
Vaidupatel <100674522+Vaidupatel@users.noreply.github.com>
2023-01-23 19:52:05 +0530

    Update 031 Preprocessor.c

--- 031 Preprocessor.c
+++ 031 Preprocessor.c
@@ -1,17 +1,17 @@
-/*
-PREPROCESSOR
+#include<stdio.h>
+#include"pointer.c"
+#define pi 3.14
+#define square(r) r*r
+int main(){
 
--->Pre-processor comes under action before the actual compilation process.
--->Pre-processor is not a part of compiler.
--->It is a text substitution tool.
--->All preprocessor commands begin with a hash (#) symbol.
-COMMANDS EXAMPLE
-#define
-#include
-#undef
-#ifdef
-#ifndef
-#if
-#else
-#elif
-*/
+float var=pi;
+printf("This is pi %f\n",var); 
+printf("The area of square is %f\n",pi*square(10)); 
+printf("File name is %s\n",__FILE__); 
+printf("Todays date is  %s\n",__DATE__); 
+printf("Time now is %s\n",__TIME__); 
+printf("Line number is %d\n",__LINE__); 
+printf("ANSI:%d\n",__STDC__); 
+
+return 0;
+}
