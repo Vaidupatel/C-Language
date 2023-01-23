cc15da2ba3add292a7726ba72f7afeb83d0fbbb6
Vaidupatel <100674522+Vaidupatel@users.noreply.github.com>
2023-01-23 19:07:31 +0530

    Create 031 Preprocessor.c

---
+++ 031 Preprocessor.c
@@ -0,0 +1,17 @@
+/*
+PREPROCESSOR
+
+-->Pre-processor comes under action before the actual compilation process.
+-->Pre-processor is not a part of compiler.
+-->It is a text substitution tool.
+-->All preprocessor commands begin with a hash (#) symbol.
+COMMANDS EXAMPLE
+#define
+#include
+#undef
+#ifdef
+#ifndef
+#if
+#else
+#elif
+*/
