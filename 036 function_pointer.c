1ed8e8bfbd2966f68a06cdd00290013dc55d5cee
Vaidupatel <100674522+Vaidupatel@users.noreply.github.com>
2023-02-25 18:26:16 +0530

    Update 036 function_pointer.c

--- 036 function_pointer.c
+++ 036 function_pointer.c
@@ -1,16 +1,33 @@
 #include<stdio.h>
+#include<stdlib.h>
 int sum(int a,int b)
 {
 
      return a+b;
 }
-int main(){
+void hello(int (*fptr)(int,int))
+{
+printf("Hello users!\n"); 
+printf("The sum of 5 and 7 is %d\n",fptr(5,7)); 
+}
 
+void GM(int (*fptr)(int,int))
+{
+printf("Good Morning users!\n"); 
+printf("The sum of 5 and 7 is %d\n",fptr(5,7)); 
+}
+int main(){
+/*
      printf("The sum is  %d\n",sum(1,2)); 
 
-     int(*fptr)(int,int);     //Creating the function pointer
+     int(*fptr)(int,float);     //Creating the function pointer
      fptr=&sum;               //Pointing the pointer 
-     int d=(*fptr)(4,6);      //De-referencing the pointer function
+     int d=(*fptr)(4,6.0);      //De-referencing the pointer function
      printf("The value of d is %d\n",d); 
+*/
+int(*ptr)(int,int);
+ptr=sum;            //We can write & or not, because in function pointer it will be same
+GM(sum);
+
 return 0;
 }
